f2cff1ce4cb4c2701e2a319845f3f7feaa55b605
Sokly <hengsokly23@gmail.com>
2018-06-15 09:32:26 +0700

    wip fix tag

--- app/controllers/api/v1/tags/stories_controller.rb
+++ app/controllers/api/v1/tags/stories_controller.rb
@@ -2,16 +2,14 @@
 
 module Api
   module V1
-    module Tags
-      class StoriesController < ApiController
-        skip_before_action :authenticate_with_token!
+    class Tags::StoriesController < ApiController
+      skip_before_action :authenticate_with_token!
 
-        def index
-          @tag = Tag.find(params[:tag_id])
-          @stories = @tag.stories.published.includes(:user, :tags).order('created_at desc')
+      def index
+        @tag = Tag.find(params[:tag_id])
+        @stories = @tag.stories.published.includes(:user, :tags).order('created_at desc')
 
-          render json: @stories, status: :ok
-        end
+        render json: @stories, status: :ok
       end
     end
   end
