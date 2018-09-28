4a63db5c37e9df4621a9d36582c78be38f501adb
Sokly <hengsokly23@gmail.com>
2018-09-28 15:22:41 +0700

    user: send email for signup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,20 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  # Change mail delvery to either :smtp, :sendmail, :file, :test
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
+  }
+
+  config.action_mailer.default_url_options = {host: "localhost:3000"}
 
   config.action_mailer.perform_caching = false
 
