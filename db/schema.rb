b31f562efc010da1836ead8105b8fb48046fa516
Sokly <hengsokly23@gmail.com>
2018-10-02 17:00:31 +0700

    add migration add column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180608014000) do
+ActiveRecord::Schema.define(version: 20181002095651) do
 
   create_table "choices", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "label"
