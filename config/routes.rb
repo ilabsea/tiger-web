7a51a947edecbb607d4b2dd90be18e479102c08d
Sokly <hengsokly23@gmail.com>
2018-10-16 12:05:15 +0700

    add authentication for sidekiq production

--- config/routes.rb
+++ config/routes.rb
@@ -32,5 +32,11 @@ Rails.application.routes.draw do
 
   post 'create_user' => 'users#create', as: :create_user
 
-  mount Sidekiq::Web => '/api/v1/sidekiq'
+
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+  end if Rails.env.production?
+
+  mount Sidekiq::Web, at: "/api/v1/sidekiq"
 end
