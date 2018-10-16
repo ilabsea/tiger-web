a330951ef1fc722d8e29fd3e5a570a7aff2d1495
Sokly <hengsokly23@gmail.com>
2018-10-16 15:36:07 +0700

    add user type in story download template

--- app/models/story_download.rb
+++ app/models/story_download.rb
@@ -9,6 +9,7 @@
 #  device_type :string(255)
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#  user_type   :string(255)
 #
 
 class StoryDownload < ApplicationRecord
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,11 @@
 #  last_sign_in_at        :datetime
 #  current_sign_in_ip     :string(255)
 #  last_sign_in_ip        :string(255)
+#  status                 :string(255)
+#  confirmation_token     :string(255)
+#  confirmed_at           :datetime
+#  confirmation_sent_at   :datetime
+#  unconfirmed_email      :string(255)
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #  role                   :integer
