03c596205bcd83c38cb8162cf43993d2c63eb10b
Sokly <hengsokly23@gmail.com>
2018-10-26 10:08:34 +0700

    fix rubocop

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,16 +6,16 @@ class HomeController < ApplicationController
   def download_web_guide
     send_file(
       "#{Rails.root}/public/web_guide.pdf",
-      filename: "web_guide.pdf",
-      type: "application/pdf"
+      filename: 'web_guide.pdf',
+      type: 'application/pdf'
     )
   end
 
   def download_mobile_guide
     send_file(
       "#{Rails.root}/public/mobile_guide.pdf",
-      filename: "mobile_guide.pdf",
-      type: "application/pdf"
+      filename: 'mobile_guide.pdf',
+      type: 'application/pdf'
     )
   end
 end
