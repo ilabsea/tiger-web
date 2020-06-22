06008e9794f203dd8656550291b70bd978392c97
Sokly <hengsokly23@gmail.com>
2020-06-22 13:41:16 +0700

    fix spec

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV['SETTINGS__SMTP__DEFAULT_FROM']
+  default from: ENV.fetch('SETTINGS__SMTP__DEFAULT_FROM') {'from@example.com'}
   layout 'mailer'
 end
